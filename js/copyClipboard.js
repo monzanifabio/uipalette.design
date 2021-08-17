9c1d81bd50c0facde7aaad91d6e51740853cf098
FabioMonzani <renderbros@gmail.com>
2021-08-17 23:40:53 +0100

    Copy CSS code and close modal

--- js/copyClipboard.js
+++ js/copyClipboard.js
@@ -10,8 +10,32 @@ clipboard.on('success', function(e) {
     sound.play();
     //Show toast notification
     toast.show()
+    $('#toastColor').css("display", 'block');
     $('#toastColor').css("backgroundColor", e.text);
     $('#toastText').text(e.text + " copied to clipboard");
+
+    e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
+
+var clipboard = new ClipboardJS('.copyCss');
+
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+    //Play copy sound
+    var sound = document.getElementById("picker");
+    sound.play();
+    //Show toast notification
+    toast.show()
+    $('#toastColor').css("display", 'none');
+    $('#toastText').text("CSS code copied to clipboard");
+    $('#gradientCssModal').modal('hide');
 
     e.clearSelection();
 });
