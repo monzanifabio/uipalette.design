579419dbeae499af916d0b2a5ff38e30c8b33ca2
FabioMonzani <renderbros@gmail.com>
2021-08-18 16:31:27 +0100

    Show color palette in the modal header

--- js/getMonoTest.js
+++ js/getMonoTest.js
@@ -4,6 +4,10 @@ function getMonoTest(el) {
   var b = $(el).attr('data-b');
   var c = $(el).attr('data-c');
   console.log(a,b,c);
+  $('#color-a').css('background-color', a);
+  $('#color-b').css('background-color', b);
+  $('#color-c').css('background-color', c);
+
   $('.mono-test-primary').css('fill', a);
   $('.mono-test-primary-text').css('fill', b);
   $('.mono-test-accent').css('fill', c);
