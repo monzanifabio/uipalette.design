1132052add9b466dee7280eeaf0bddddb83cb19b
FabioMonzani <renderbros@gmail.com>
2021-10-12 01:01:42 +0100

    Added npm purgecss and cssnano

---
+++ purgecss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    '*.php',
+    'include/*.php'
+  ],
+  css: [
+    'vendors/bootstrap-4.5.0/css/bootstrap.min.css',
+    'vendors/fontawesome/duotone.min.css',
+    'vendors/fontawesome/fontawesome.min.css',
+    'vendors/fontawesome/brands.min.css'
+  ]
+}
