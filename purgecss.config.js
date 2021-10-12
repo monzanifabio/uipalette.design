0bc658d6a73dadd2a0fede40dc0c561865ddaa9d
FabioMonzani <renderbros@gmail.com>
2021-10-12 01:02:40 +0100

    Purge css config update

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   content: [
-    '*.php',
-    'include/*.php'
+    '*.html'
   ],
   css: [
-    'vendors/bootstrap-4.5.0/css/bootstrap.min.css',
-    'vendors/fontawesome/duotone.min.css',
-    'vendors/fontawesome/fontawesome.min.css',
-    'vendors/fontawesome/brands.min.css'
+    'css/style.css'
   ]
 }
